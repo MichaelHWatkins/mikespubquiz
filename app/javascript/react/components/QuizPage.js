ce154915b2ad0afbd9781e2c20159c0c92455568
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-01-07 18:57:36 -0500

    added a tile and still need to fix a bug in the code.

--- app/javascript/react/components/QuizPage.js
+++ app/javascript/react/components/QuizPage.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import data from './data.json'
+import QuizPageTile from './QuizPageTile'
 
 const QuizPage = () => {
+  const [quizList, setQuizList] = useState([]);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  useEffect(() => {
+    data.sort(() => Math.random() - 0.5);
+    const list = data.slice(0, 10)
 
-  let quizList = []
-  let questionNumber = 0
-
-  data.sort(() => Math.random() - 0.5);
-  quizList = data.slice(0, 10)
-
-  const newList = quizList.map((quizInfo) => {
-
-   quizInfo.incorrect.push(quizInfo.correct);
-   quizInfo.incorrect.sort(() => Math.random() - 0.5);
-   return quizInfo
-  })
-
+      const newList = list.map((quizInfo) => {
+        quizInfo.incorrect.push(quizInfo.correct);
+        quizInfo.incorrect.sort(() => Math.random() - 0.5);
+        setQuizList(quizList.push(quizInfo))
+      })
+    
+  },[])
 
   return(
+
     <div>
-      <p>{newList[questionNumber].question}</p>
-      <input type="radio" value="answer1" name="answer" /> {newList[questionNumber].incorrect[0]}
-      <input type="radio" value="answer2" name="answer" /> {newList[questionNumber].incorrect[1]}
-      <input type="radio" value="answer3" name="answer" /> {newList[questionNumber].incorrect[2]}
-      <input type="radio" value="answer4" name="answer" /> {newList[questionNumber].incorrect[3]}
+    <QuizPageTile
+      quizList={quizList}
+      questionNumber={questionNumber}
+    />
     </div>
   );
+
 }
 
 export default QuizPage
