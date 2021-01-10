7b0696c73c7a2a3e59c0a3aee26e501af284569c
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-01-09 22:29:47 -0500

    got the answers to dynamically appear!

--- app/javascript/react/components/QuizPage.js
+++ app/javascript/react/components/QuizPage.js
@@ -1,32 +1,52 @@
 import React, { useState, useEffect } from 'react'
+import _ from 'lodash';
 import data from './data.json'
-import QuizPageTile from './QuizPageTile'
-
 const QuizPage = () => {
   const [quizList, setQuizList] = useState([]);
   const [questionNumber, setQuestionNumber] = useState(0);
   useEffect(() => {
-    data.sort(() => Math.random() - 0.5);
-    const list = data.slice(0, 10)
+    const deep = _.cloneDeep(data)
 
+    deep.sort(() => Math.random() - 0.5);
+
+    const list = deep.slice(0, 10)
       const newList = list.map((quizInfo) => {
         quizInfo.incorrect.push(quizInfo.correct);
         quizInfo.incorrect.sort(() => Math.random() - 0.5);
-        setQuizList(quizList.push(quizInfo))
+
+        return quizInfo;
+
       })
-    
+
+      setQuizList([...quizList, newList])
   },[])
 
-  return(
+    if ( quizList.length === 0) {
+    return(
+      <div>
+      test
+      </div>
+    )
+  }
 
-    <div>
-    <QuizPageTile
-      quizList={quizList}
-      questionNumber={questionNumber}
-    />
-    </div>
-  );
+  else {
+    return(
+      <div>
+      <form className= 'callout'>
+        <p>{quizList[0][questionNumber].question}</p>
 
+        {quizList[0][questionNumber].incorrect.map(incorrectAnswer => (
+          <li>{incorrectAnswer}</li>
+        ))}
+          <button onClick={(event) => {
+            event.preventDefault();
+            setQuestionNumber(questionNumber + 1)
+          }}>
+          Click me
+        </button>
+      </form>
+      </div>
+    );
+  }
 }
-
 export default QuizPage
